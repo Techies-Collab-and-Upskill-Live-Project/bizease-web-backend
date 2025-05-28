873004981ff76b207952e002c9aac8675d4f56c2
Adedamola Egbebi <egbebitim121@gmail.com>
2025-05-28 09:04:50 +0100

    Update CustomUser model

--- bizease/accounts/models.py
+++ bizease/accounts/models.py
@@ -37,6 +37,19 @@ class CustomUserManager(BaseUserManager):
 		return self.create_user(email, password, **extra_fields)
 
 
+BUSINESS_CHOICES = [
+	("Sole proprietorship","Sole proprietorship"),
+	("General partnership","General partnership"),
+	("Limited partnership","Limited partnership"),
+	("Limited liability partnership (LLP)","Limited liability partnership (LLP)"),
+	("C corporation","C corporation"),
+	("S corporation","S corporation"),
+	("Benefit corporation","Benefit corporation"),
+	("Limited liability company (LLC)","Limited liability company (LLC)"),
+	("Nonprofit","Nonprofit"),
+	("Joint venture","Joint venture")
+]
+
 class CustomUser(AbstractBaseUser, PermissionsMixin):
 	business_name = models.CharField(max_length=200, unique=True)
 	full_name = models.CharField(max_length=200)
@@ -44,11 +57,12 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 	business_email = models.CharField(max_length=150, unique=True, blank=True, null=True)
 	CURRENCY_CHOICES = {"USD": "United States dollar", "NGN": "Nigerian naira", "GBP": "Pound sterling"}
 	currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default="NGN")
-	business_type = models.CharField(max_length=150, blank=True)
+	business_type = models.CharField(max_length=150, blank=True, choices=BUSINESS_CHOICES)
 	password = models.CharField(max_length=50)
 	COUNTRY_CHOICES = {"United States": "United States", "Nigeria": "Nigeria"}
 	country = models.CharField(choices=COUNTRY_CHOICES, default="Nigeria");
 	state = models.CharField(max_length=100, blank=True)
+	low_stock_threshold = models.IntegerField(default=0)
 	date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
 	is_staff = models.BooleanField(default=False)
 	is_active = models.BooleanField(default=True)
@@ -57,7 +71,9 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 	REQUIRED_FIELDS = []
 	objects = CustomUserManager()
 
-	# users table would need some form of low_stock_alert_threshold col if we are following design then
+	class Meta:
+		constraints = [models.CheckConstraint(condition=models.Q(low_stock_threshold__gte=0), name="low_stock_threshold_gte_0")]
+
 	# all requests should have some form of currency field then
 
 	def __str__(self):
