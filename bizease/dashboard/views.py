b72e37e781c550b862dc159e6204cd7de14e69ea
Adedamola Egbebi <egbebitimi121@gmail.com>
2025-06-12 23:11:31 +0100

    Make dashboard view functional

--- bizease/dashboard/views.py
+++ bizease/dashboard/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from orders.models import Order, OrderedProduct
+from inventory.models import Inventory
+from rest_framework import status
+from django.db.models import Sum, F
+from orders.serializers import OrdersArraySerializers
+from inventory.serializers import InventoryListSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+"""
+- pending orders
+  - id
+  - orderId // This would be removed. Generate it with the id field
+  - customerName
+  - total
+  - date
+  - status // needed? I mean it's clearly stated that it's a pending order
+
+- low stock items
+  - id
+  - low_stock_threshold
+  - category
+  - stockLevel
+  - price
+  - lastUpdated
+  - actions: 'Restock', // This would no longer be there
+  - status: 'Low Stock', // needed? I mean it's clearly stated that it's a low stock
+ """
+
+# todo: add support for query params to specify pending_orders and low_stock_items count expected
+class DashBoardView(APIView):
+	permission_classes = [IsAuthenticated]
+
+	def get(self, request, **kwargs):
+		dashboard_data = {}
+		dashboard_data["business_name"] = request.user.business_name
+		dashboard_data["currency"] = request.user.currency
+		dashboard_data["language"] = request.user.language
+		products_orders = OrderedProduct.objects.values("name").annotate(total_units_sold=Sum("quantity")).order_by("-total_units_sold")
+
+		# top selling product - ordered product with the max sum of quantity
+		if len(products_orders) == 0:
+			dashboard_data["top_selling_product"] = None
+		else:
+			dashboard_data["top_selling_product"] = products_orders[0]['name']
+
+		# revenue - sum of total_price in orders
+		dashboard_data["revenue"] = Order.objects.aggregate(Sum("total_price"))['total_price__sum']
+
+		orders_serializer = OrdersArraySerializers(
+			{"data": list(Order.objects.filter(product_owner_id=request.user.id).filter(status="Pending").order_by("-order_date")[:6])}
+		)
+		inventory_serializer = InventoryListSerializer(
+			{"data": list(Inventory.objects.filter(owner=request.user.id).filter(stock_level__lte=F("low_stock_threshold")).order_by("-last_updated")[:6])}
+		)
+		dashboard_data["pending_order"] = orders_serializer.data["data"]
+		dashboard_data["low_stock_items"] = inventory_serializer.data["data"]
+		return Response(dashboard_data, status=status.HTTP_200_OK)
+
+		# dashboard_data["top_selling_product"] = OrderedProduct.objects.values("name").annotate(total_units_sold=Sum("quantity"))
