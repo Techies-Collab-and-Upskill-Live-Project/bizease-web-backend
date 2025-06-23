37369c4f369e25a6e0d45e70d092f8c2c410679c
Adedamola Egbebi <egbebitimi121@gmail.com>
2025-06-23 19:52:42 +0100

    Add proper unique constraint on the 'product_name' field of Inventory model
    Removed the unique constraint that enforced the uniqueness of an inventory
    item's 'product_name' across the entrie database and replaced it with a
    constraint that enforces 'product_name' uniqueness among the products
    in a User's inventory only.

--- bizease/inventory/models.py
+++ bizease/inventory/models.py
@@ -3,7 +3,7 @@ from accounts.models import CustomUser
 
 class Inventory(models.Model):
 	owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-	product_name = models.CharField(max_length=100, unique=True)
+	product_name = models.CharField(max_length=100)
 	description = models.CharField(max_length=300, blank=True)
 	category = models.CharField(max_length=100, blank=True)
 	stock_level = models.PositiveIntegerField(default=0)
@@ -14,6 +14,7 @@ class Inventory(models.Model):
 
 	class Meta:
 		ordering = ["-last_updated"]
+		constraints = [models.UniqueConstraint(fields=["owner", "product_name"], name="user_unique_product")]
 
 	def __str__(self):
 		return f"{self.product_name} - {self.price}"
