421ef41b7c3aa5552284b9a6e16797119c28501a
edarko28 <evans.darko700@gmail.com>
2025-06-22 12:02:29 -1200

    Add new change

--- bizease/reports/urls.py
+++ bizease/reports/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', DashboardView.as_view(), name='dashboard'),
     path('reports/', ReportListCreateView.as_view(), name='create-list-reports'),
     path('reports/<int:pk>/', ReportDetailView.as_view(), name='detail-report'),
-    path('reports/<int:pk>/download/', ReportDownloadView.as_view(), name='download-report')
+    path('reports/<int:pk>/download/', ReportDownloadView.as_view(), name='download_report')
 ]
