443a612583dce63ceae86206dc6ce9d8e8e4bd31
Adedamola Egbebi <egbebitimi121@gmail.com>
2025-06-30 02:51:57 +0100

    Refactor the code

--- bizease/orders/serializers.py
+++ bizease/orders/serializers.py
@@ -2,17 +2,35 @@ from rest_framework import serializers
 from .models import Order, OrderedProduct
 from inventory.models import Inventory
 from datetime import datetime
+from django.db.utils import IntegrityError
+from decimal import Decimal
+from django.utils import timezone
+
+def validate_decimal(value):
+	if type(value) != int and type(value) != float and type(value) != Decimal:
+		raise serializers.ValidationError("Value must be a float or an integer")
+
+def validate_int(value):
+	if type(value) != int:
+		raise serializers.ValidationError("Value must be an integer")
+
 
 class OrderedProductSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = OrderedProduct
 		fields = [
-			'name', 'quantity', 'price'
+			'id', 'name', 'quantity', 'price', "cummulative_price"
 		]
 
-"""
-{"client_name": "customer1", "client_email": "customer1@gmail.com", "status": "Pending", "ordered_products": [{"name": "NKJV Bible", "quantity": 3, "price": 5000}]}
-"""
+	cummulative_price = serializers.DecimalField(max_digits=14, decimal_places=2, required=False)
+	quantity = serializers.IntegerField(min_value=1, validators=[validate_int])
+	price = serializers.DecimalField(max_digits=14, decimal_places=2, validators=[validate_decimal])
+	read_only_fields = ['id', 'name', 'price', 'cummulative_price']
+
+	def update(self):
+		self.instance.quantity = self.validated_data["quantity"]
+		self.instance.save()
+
 
 class OrderSerializer(serializers.ModelSerializer):
 	class Meta:
@@ -25,7 +43,6 @@ class OrderSerializer(serializers.ModelSerializer):
 	ordered_products = OrderedProductSerializer(many=True)
 	read_only_fields = ['id', 'delivery_date']
 
-	# todo: Wrap it all in a transaction
 	def create(self, product_owner):
 		ordered_products = self.validated_data["ordered_products"]
 		if len(ordered_products) == 0:
@@ -34,73 +51,18 @@ class OrderSerializer(serializers.ModelSerializer):
 		del self.validated_data["ordered_products"] # ordered_products is not a column in Order table
 		new_order = Order(**self.validated_data)
 		new_order.product_owner_id = product_owner
+		new_order.ordered_products_objects = []
 
-		# Dictionary mapping available product name to an OrderedProducts instance with the same name
-		ordered_products_dict = {}
+		for item in ordered_products:
+			new_order.ordered_products_objects.append(OrderedProduct(**item))
 
-		for product in ordered_products:
-			product["name"] = product["name"].title() # normalization
-			if not ordered_products_dict.get(product["name"]): # This product hasn't been previously encountered in the list
-				ordered_products_dict[product["name"]] = OrderedProduct(
-					name=product["name"], price=product["price"],
-					quantity=product["quantity"]
-				)
-			else: # The same product was added to the order more than once. We don't want that, That's why we have a quantity field
-				return {"detail": [f"Duplicate Ordered Product: {product['name']}. Use the quantity field to specify multiple items"], "status": 400}
+		errors = new_order.save()
+		if (errors):
+			return {"errors": errors}
+		return {"data": new_order}
 
-		available_products = Inventory.objects.filter(owner_id=product_owner.id).filter(product_name__in=list(ordered_products_dict.keys()))
-		if len(available_products) == 0:
-			return {"detail": [f"Ordered items aren't in the inventory!"], "status": 400}
-		errors = []
-		for item in available_products:
-			ordered_product = ordered_products_dict.get(item.product_name)
-			if not ordered_product:
-				errors.append(f"'{item.product_name}' Doesn't exist in the Inventory.")
-			if ordered_product.quantity > item.stock_level:
-				errors.append(f"Not enough products in stock to satisfy order for '{item.product_name}'")
-			if ordered_product.price != item.price:
-				errors.append(f"Price isn't the same as that of inventory item '{item.product_name}'")
-
-			item.stock_level -= ordered_product.quantity 
-
-		if errors:
-			return {"detail": errors, "status": 400}
-
-		new_order.save()
-		for ordered_product in ordered_products_dict.values():
-			ordered_product.cummulative_price=ordered_product.price * ordered_product.quantity
-			ordered_product.order_id = new_order
-			ordered_product.save()
-			new_order.total_price += ordered_product.cummulative_price
-
-		for item in available_products:
-			item.save()
-
-		new_order.save()
-			
-		return {"detail": "Order created successfully", "status": 200}
 
 	def update(self, product_owner):
-		"""
-		Non editable fields (Order)
-		product_owner_id
-		total_price
-		order_date
-
-		Non editable fields (OrderedProducts)
-		order_id
-		cummulative_price
-
-		Editable fields
-		client_name
-		client_email
-		client_phone
-		status
-
-		name
-		quantity
-		price
-		"""
 		if self.instance.status != "Pending":
 			return {"detail": "Only pending orders can be edited", "status": 400}
 
@@ -110,10 +72,12 @@ class OrderSerializer(serializers.ModelSerializer):
 		self.instance.status = self.validated_data.get('status', self.instance.status)
 
 		if self.instance.status == "Delivered":
-			self.instance.delivery_date = datetime.now()
+			self.instance.delivery_date = timezone.now()
 
-		self.instance.save()
-		return {"detail": "Order Updated successfully", "status": 200}
+		errors = self.instance.save()
+		if (errors):
+			return {"errors": errors}
+		return {"data": self.instance}
 
 
 	def save(self, product_owner):
