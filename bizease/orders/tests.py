1f07b5f2544262630ce0ea3b07c1ef386e4aa96a
Adedamola Egbebi <egbebitimi121@gmail.com>
2025-07-03 21:18:56 +0100

    Add Orders views unit tests

--- bizease/orders/tests.py
+++ bizease/orders/tests.py
@@ -1,9 +1,14 @@
+from rest_framework.test import APITestCase
 from django.test import TestCase
 from .models import Order, OrderedProduct
 from .serializers import OrderedProductSerializer, OrderSerializer
 from accounts.models import CustomUser
 from inventory.models import Inventory
 from django.db.utils import IntegrityError
+from django.urls import reverse
+from django.utils import timezone
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class OrderModelTest(TestCase):
@@ -25,7 +30,6 @@ class OrderModelTest(TestCase):
 		cls.order_1.ordered_products_objects = [cls.item_1, cls.item_2]
 		cls.order_1.save()
 		# test max_length is enforced
-		# test that nothing is actually saved to the db if any error occurs or something
 
 	def test_save_new_order_instance(self):
 		order_1 = Order(product_owner_id=self.test_user, client_name="customer1", client_email="customer1@gmail.com", client_phone="08149672890")
@@ -104,21 +108,6 @@ class OrderModelTest(TestCase):
 		self.assertEqual(self.order_1.total_price, 2100)
 		self.assertEqual(Inventory.objects.get(pk=self.product_1.id).stock_level, 470)
 
-
-# Things to test
-# Orders Model
-# ordered_product_serializer
-# order serializer
-# Creating new order with the serializer
-# Updating existing order with the Serializer
-# Add new product to an Order
-# Delete product from an Order
-# Introduce errors on purpose to test how well the transactions work
-# Is there no need for a tearDown method
-
-# class OrderedProductModelTest(TestCase):
-
-# 	def test_save_new_model_instance(self):
 
 class OrderSerializersTest(TestCase):
 	@classmethod
@@ -273,3 +262,245 @@ class OrderSerializersTest(TestCase):
 		self.assertRegex(updated_order.order_date.isoformat().replace('+00:00', 'Z'), r'^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{6}Z$')
 		self.assertRegex(updated_order.delivery_date.isoformat().replace('+00:00', 'Z'), r'^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{6}Z$')
 		self.assertEqual(updated_order.ordered_products.count(), 3)
+
+
+class OrdersViewsTest(APITestCase):
+	@classmethod
+	def setUpTestData(cls):
+		cls.test_user = CustomUser.objects.create(
+			business_name="user-biz", full_name="test user", email="testuser123@gmail.com", password="12345678"
+		)
+		cls.test_user_2 = CustomUser.objects.create(
+			business_name="rocket-biz", full_name="rocket monkey", email="rocketmonkey@gmail.com", password="12345678"
+		)
+		cls.refresh_obj = RefreshToken.for_user(cls.test_user)
+		# refresh_token = str(refresh_obj)
+		cls.access_token = str(cls.refresh_obj.access_token)
+
+		cls.item_1 = Inventory.objects.create(owner=cls.test_user, product_name="Calculator", price=10000, stock_level=100)
+		cls.item_2 = Inventory.objects.create(owner=cls.test_user, product_name="Safety Boots", price=65000, stock_level=20)
+		cls.item_3 = Inventory.objects.create(owner=cls.test_user, product_name="Helmet", price=6000, stock_level=45)
+
+		cls.test_order = Order(product_owner_id=cls.test_user, client_name="bob", client_email="bob@gmail.com")
+		cls.ordered_product_1 = OrderedProduct(name="Calculator", quantity=1, price=10000)
+		cls.ordered_product_2 = OrderedProduct(name="Helmet", quantity=5, price=6000)
+		cls.test_order.ordered_products_objects = [cls.ordered_product_1, cls.ordered_product_2]
+		cls.test_order.save()
+
+
+	def create_valid_new_order_req(self):
+		data = {
+			"client_name": "client1",
+			"client_email": "clientemail@gmail.com",
+			"client_phone": "08048672894",
+			"ordered_products": [
+				{"name": "Helmet", "quantity": 40, "price": 6000},
+				{"name": "calculator", "quantity": 10, "price": 10000} # test product name normalization
+			]
+		}
+
+		response = self.client.post(reverse("orders", args=["v1"]), data)
+		self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+		self.assertEqual(response.data["detail"], "Order created successfully")
+		new_order = Order.objects.get(pk=response.data["data"]["id"])
+		self.assertEqual(340000, new_order.total_price)
+		self.assertEqual(2, new_order.ordered_products.count())
+		self.assertEqual("Helmet", new_order.ordered_products.all()[0].name)
+		self.assertEqual("Calculator", new_order.ordered_products.all()[1].name)
+
+	def create_order_req_with_incomplete_data(self):
+		data = {
+			"ordered_products": [
+				{
+					"quantity": 5.5,
+					"price": 100,
+				},
+				{
+					"name": "Helmet",
+					"quantity": 0,
+				},
+				{
+					"name": "Hen",
+					"price": 5000
+				},
+			]
+		}
+		response = self.client.post(reverse("orders", args=["v1"]), data)
+
+		self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+		self.assertEqual(response.data["detail"]['client_name'][0], 'This field is required.')
+		self.assertEqual(response.data["detail"]['ordered_products'][0]['name'][0], 'This field is required.')
+		self.assertEqual(response.data["detail"]['ordered_products'][1]['price'][0], 'This field is required.')
+		self.assertEqual(response.data["detail"]['ordered_products'][2]['quantity'][0], 'This field is required.')
+
+	def create_order_req_for_missing_inventory_item(self):
+		data = {
+			"client_name": "person",
+			"client_email": "person@gmail.com",
+			"client_phone": "09018372693",
+			"ordered_products": [
+				{
+					"name": "Wisdom",
+					"quantity": 40,
+					"price": 6000,
+				},
+				{
+					"name": "calculator",
+					"quantity": 10,
+					"price": 10000,
+				},
+				{
+					"name": "Helmet",
+					"quantity": 50,
+					"price": 6300,
+				}
+			]
+		}
+
+		response = self.client.post(reverse("orders", args=["v1"]), data)
+		self.assertEqual(response.data["detail"], {"Wisdom": ["'Wisdom' doesn't exist in the Inventory"]})
+		err_obj = {
+			"Wisdom": [
+				"'Wisdom' doesn't exist in the Inventory",
+				"Ordered products must be unique. Use the quantity field to specify multiple orders of same item."
+			],
+			"Calculator": ["Price isn't the same as that of inventory item for 'Calculator'"],
+			"Helmet": [
+				"Price isn't the same as that of inventory item for 'Helmet'",
+				"Not enough products in stock to satisfy order for 'Helmet'"
+			],
+		}
+		response = self.client.post(reverse("orders", args=["v1"]), data)
+		self.assertEqual(response.data["detail"], err_obj)
+		self.assertRaises(Order.DoesNotExist, Order.objects.get, client_name="person")
+
+		
+	def create_order_req_with_non_unique_ordered_product_data(self):
+		print("==>", Inventory.objects.get(product_name="Helmet").stock_level)
+		data = {
+			"client_name": "customer",
+			"client_email": "customer@email.com",
+			"ordered_products": [
+				{
+					"name": "Helmet",
+					"quantity": 1,
+					"price": 6000,
+				},
+				{
+					"name": "Helmet",
+					"quantity": 10,
+					"price": 6000,
+				}
+			]
+		}
+		response = self.client.post(reverse("orders", args=["v1"]), data)
+		self.assertEqual(response.data["detail"], {
+			"Helmet": ["Ordered products must be unique. Use the quantity field to specify multiple orders of same item."]
+		})
+		self.assertRaises(Order.DoesNotExist, Order.objects.get, client_name="customer")
+
+
+	def test_create_order_reqs_with_credentials(self):
+		self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.access_token)
+		self.create_order_req_with_incomplete_data()
+		# self.create_valid_new_order_req()
+		self.create_order_req_with_non_unique_ordered_product_data()
+		# self.create_order_req_for_missing_inventory_item()
+
+	def test_create_order_req_without_credentials(self):
+		response = self.client.post(reverse("orders", args=["v1"]), {"product_name": "product-1", "stock_level": 5, "price": 800})
+		self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+	def get_orders_data_filter_status_req_(self):
+		pass
+	def get_orders_paginated_data_req(self):
+		pass
+	def get_orders_search_req(self):
+		pass
+	def get_orders_req_for_ordered_data(self):
+		pass
+
+	def test_get_orders_with_credentials(self):
+		self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.access_token)
+		response = self.client.get(reverse("orders", args=["v1"]))
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+		self.assertEqual(response.data["data"]["page_count"], 1)
+		self.assertEqual(response.data["data"]["next_page"], None)
+		self.assertEqual(response.data["data"]["prev_page"], None)
+		order_count = Order.objects.filter(product_owner_id=self.test_user.id).count()
+		self.assertEqual(1, order_count)
+		self.assertEqual(response.data["data"]["length"], order_count)
+		# Test the products returned but it seems like we need to set default ordering first
+		# Test the query params firsts
+
+	def test_get_orders_without_credentials(self):
+		response = self.client.get(reverse("orders", args=["v1"]))
+		self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+	def test_get_single_order_with_credentials(self):
+		self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.access_token)
+		response = self.client.get(reverse("order", args=["v1", str(self.test_order.id)]))
+		expected_data = OrderSerializer(self.test_order).data
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+		self.assertEqual(response.data["data"], expected_data)
+
+		response = self.client.get(reverse("order", args=["v1", '99999999999']))
+		self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+		self.assertEqual(response.data["detail"], "Order not found")
+
+	def test_get_single_inventory_item_without_credentials(self):
+		response = self.client.get(reverse("order", args=["v1", "1"]))
+		self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+	def update_order_with_valid_data(self):
+		order = Order(product_owner_id=self.test_user, client_name="Tim", client_email="timilehin@tmail.com")
+		ordered_product_1 = OrderedProduct(name="Safety Boots", quantity=1, price=65000)
+		ordered_product_2 = OrderedProduct(name="Helmet", quantity=5, price=6000)
+		order.ordered_products_objects = [ordered_product_1, ordered_product_2]
+		order.save()
+
+		self.assertEqual(order.client_name, "Tim")
+		self.assertRegex(order.order_date.isoformat().replace('+00:00', 'Z'), r'^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{6}Z$')
+		self.assertEqual(order.delivery_date, None)
+
+		update_payload = {
+			"client_name": "sam",
+			"client_email": "sam45@gmail.com",
+			"client_phone": "07014537372",
+			"status": "Delivered"
+		}
+
+		response = self.client.put(reverse("order", args=["v1", str(order.id)]), update_payload)
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+		order = Order.objects.get(pk=order.id)
+		self.assertEqual(order.client_name, "sam")
+		self.assertEqual(order.client_email, "sam45@gmail.com")
+		self.assertEqual(order.client_phone, "07014537372")
+		self.assertRegex(order.status, "Delivered")
+		self.assertRegex(order.delivery_date.isoformat().replace('+00:00', 'Z'), r'^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{6}Z$')
+
+	def test_update_single_inventory_item_with_credentials(self):
+		self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.access_token)
+		self.update_order_with_valid_data()
+		# self.update_item_with_invalid_data
+
+	def test_delete_order_with_credentials(self):
+		self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.access_token)
+
+		order_to_delete = Order(product_owner_id=self.test_user, client_name="Damola")
+		ordered_product = OrderedProduct(name="Safety Boots", quantity=1, price=65000)
+		order_to_delete.ordered_products_objects = [ordered_product]
+		order_to_delete.save()
+
+		response = self.client.delete(reverse("order", args=["v1", str(order_to_delete.id)]))
+		self.assertRaises(Order.DoesNotExist, Order.objects.get, pk=self.item_3.id)
+		self.assertEqual(response.data["detail"], "Order deleted successfully")
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+		response = self.client.delete(reverse("order", args=["v1", "999999999"]))
+		self.assertEqual(response.data["detail"], "Order not found")
+		self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+	def test_delete_order_without_credentials(self):
+		response = self.client.delete(reverse("order", args=["v1", '3']))
+		self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
