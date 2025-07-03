fbf6c40031f2f63bac3a14ce627d54b6998f2d30
Adedamola Egbebi <egbebitimi121@gmail.com>
2025-07-03 21:20:54 +0100

    Add 'name' kwarg to the urlconfig paths

--- bizease/inventory/urls.py
+++ bizease/inventory/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-	path('', views.InventoryView.as_view()),
-	path('stats', views.InventoryStatsView.as_view()),
-	path('<int:item_id>', views.InventoryItemView.as_view()),
+	path('', views.InventoryView.as_view(), name="inventory"),
+	path('stats', views.InventoryStatsView.as_view(), name="inventory-stats"),
+	path('<int:item_id>', views.InventoryItemView.as_view(), name="inventory-item"),
 ]
