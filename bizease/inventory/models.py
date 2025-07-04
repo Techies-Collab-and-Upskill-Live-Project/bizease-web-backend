a99be8aec3838b7279f9c8adbb06d1fbfdaa681a
Adedamola Egbebi <egbebitimi121@gmail.com>
2025-07-04 03:01:30 +0100

    Add new constraint to the Inventory model's price field
    The new constraint makes sure the price is always greater than zero

--- bizease/inventory/models.py
+++ bizease/inventory/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from accounts.models import CustomUser
+from django.db.models import Q
 
 class Inventory(models.Model):
 	owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
@@ -7,16 +8,17 @@ class Inventory(models.Model):
 	description = models.CharField(max_length=300, blank=True)
 	category = models.CharField(max_length=100, blank=True)
 	stock_level = models.PositiveIntegerField(default=0)
-	low_stock_threshold = models.PositiveIntegerField(default=5)
-	
-	# Add constraint such that the field only ever approaches zero as it reduces
+	low_stock_threshold = models.PositiveIntegerField(default=5)	
 	price = models.DecimalField(default=0, max_digits=14, decimal_places=2)
 	last_updated = models.DateTimeField(auto_now=True)
 
 
 	class Meta:
 		ordering = ["-last_updated"]
-		constraints = [models.UniqueConstraint(fields=["owner", "product_name"], name="user_unique_product")]
+		constraints = [
+			models.UniqueConstraint(fields=["owner", "product_name"], name="user_unique_product"),
+			models.CheckConstraint(condition=Q(price__gt=0), name="price_greater_than_zero")
+		]
 
 	def __str__(self):
 		return f"{self.product_name} - {self.price}"
