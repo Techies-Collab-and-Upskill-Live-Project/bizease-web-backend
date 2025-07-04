8068cec29131e346bf7bd77ccc68a7b1f5cd4b6f
Adedamola Egbebi <egbebitimi121@gmail.com>
2025-07-04 03:09:22 +0100

    Add new paths and added 'name' kwarg to existing path

--- bizease/orders/urls.py
+++ bizease/orders/urls.py
@@ -2,7 +2,9 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-	path('', views.OrdersView.as_view()),
-	path('stats', views.OrderStatsView.as_view()),
-	path('<int:item_id>', views.SingleOrderView.as_view()),
+	path('', views.OrdersView.as_view(), name="orders"),
+	path('stats', views.OrderStatsView.as_view(), name="orders-stats"),
+	path('<int:order_id>', views.SingleOrderView.as_view(), name="order"),
+	path('<int:order_id>/ordered-products/<int:product_id>', views.OrderedProductView.as_view(), name="ordered-products"),
+	path('<int:order_id>/ordered-products', views.OrderedProducts.as_view(), name="ordered-product"),
 ]
