193d081c067cbaa4603e7ee15ab1490539c9814b
Adedamola Egbebi <egbebitimi121@gmail.com>
2025-07-08 16:45:18 +0100

    Increase the number of currency and country choices for a user

--- bizease/accounts/models.py
+++ bizease/accounts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.contrib.auth.base_user import BaseUserManager
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
-
+from .constants import BUSINESS_CHOICES, COUNTRY_CHOICES, CURRENCY_CHOICES
 
 class CustomUserManager(BaseUserManager):
 	def create_user(self, email, password=None, **extra_fields):
@@ -29,19 +29,6 @@ class CustomUserManager(BaseUserManager):
 		return self.create_user(email, password, **extra_fields)
 
 
-BUSINESS_CHOICES = [
-	("Sole proprietorship","Sole proprietorship"),
-	("General partnership","General partnership"),
-	("Limited partnership","Limited partnership"),
-	("Limited liability partnership (LLP)","Limited liability partnership (LLP)"),
-	("C corporation","C corporation"),
-	("S corporation","S corporation"),
-	("Benefit corporation","Benefit corporation"),
-	("Limited liability company (LLC)","Limited liability company (LLC)"),
-	("Nonprofit","Nonprofit"),
-	("Joint venture","Joint venture")
-]
-
 class CustomUser(AbstractBaseUser, PermissionsMixin):
 	business_name = models.CharField(max_length=200, unique=True)
 	full_name = models.CharField(max_length=200)
@@ -50,11 +37,9 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 	phone = models.CharField(max_length=24, blank=True)
 	business_phone = models.CharField(max_length=24, blank=True)
 	business_address = models.CharField(max_length=150, blank=True)
-	CURRENCY_CHOICES = {"USD": "United States dollar", "NGN": "Nigerian naira", "GBP": "Pound sterling"}
 	currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default="NGN")
 	business_type = models.CharField(max_length=150, blank=True, choices=BUSINESS_CHOICES)
 	password = models.CharField(max_length=128)
-	COUNTRY_CHOICES = {"United States": "United States", "Nigeria": "Nigeria"}
 	country = models.CharField(choices=COUNTRY_CHOICES, default="Nigeria");
 	state = models.CharField(max_length=100, blank=True)
 	rcv_mail_for_new_orders = models.BooleanField(default=True)
