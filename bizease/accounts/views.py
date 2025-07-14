3f4154c33e224f5768f1ff0babd766f231c53ceb
Adedamola Egbebi <egbebitimi121@gmail.com>
2025-07-14 17:53:16 +0100

    Fix indentation error

--- bizease/accounts/views.py
+++ bizease/accounts/views.py
@@ -132,31 +132,31 @@ class LoginView(APIView):
 
 
 class ProfileView(APIView):
-	permission_classes = [IsAuthenticated]
-	parser_classes = [JSONParser]
+    permission_classes = [IsAuthenticated]
+    parser_classes = [JSONParser]
 
-	def get(self, request, **kwargs):
-		userProfileDict = ProfileDataSerializer(request.user).data
-		return Response({"data": userProfileDict}, status=status.HTTP_200_OK)
+    def get(self, request, **kwargs):
+        userProfileDict = ProfileDataSerializer(request.user).data
+        return Response({"data": userProfileDict}, status=status.HTTP_200_OK)
 
-	def put(self, request, **kwargs):
-		dataUpdate = ProfileDataSerializer(request.user, data=request.data, partial=True)
-		if dataUpdate.is_valid():
-			if dataUpdate.validated_data.get("field_errors"):
-				return Response({"detail": dataUpdate.validated_data["field_errors"]}, status=status.HTTP_200_OK)
-			result = dataUpdate.save()
-			return Response({"detail": "User data updated successfully"}, status=status.HTTP_200_OK)
-		else:
-			return Response({"detail": dataUpdate.errors}, status=status.HTTP_400_BAD_REQUEST)
+    def put(self, request, **kwargs):
+        dataUpdate = ProfileDataSerializer(request.user, data=request.data, partial=True)
+        if dataUpdate.is_valid():
+            if dataUpdate.validated_data.get("field_errors"):
+                return Response({"detail": dataUpdate.validated_data["field_errors"]}, status=status.HTTP_200_OK)
+            result = dataUpdate.save()
+            return Response({"detail": "User data updated successfully"}, status=status.HTTP_200_OK)
+        else:
+            return Response({"detail": dataUpdate.errors}, status=status.HTTP_400_BAD_REQUEST)
 
-	def delete(self, request, **kwargs):
-		del_count, del_dict = request.user.delete()
-		if (del_count > 0):
-			return Response({"detail": "User data deleted successfully"}, status=status.HTTP_200_OK)
-		else: # What could go wrong?
-			return Response(
-				{"detail": "Delete operation incomplete. Something went wrong while deleting user data"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
-			)
+    def delete(self, request, **kwargs):
+        del_count, del_dict = request.user.delete()
+        if (del_count > 0):
+            return Response({"detail": "User data deleted successfully"}, status=status.HTTP_200_OK)
+        else: # What could go wrong?
+            return Response(
+                {"detail": "Delete operation incomplete. Something went wrong while deleting user data"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
 
 class LogoutView(APIView):
     permission_classes = [IsAuthenticated]
@@ -182,7 +182,7 @@ class LogoutView(APIView):
         except:
             return Response({"detail": "Something went wrong! Please try again"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         return Response({"detail": "User logged out"}, status=status.HTTP_200_OK)
-	
+    
 class PasswordResetRequestView(APIView):
     def post(self, request, **kwargs):
         email = request.data.get("email")
