32edd1e2aad3d9a3a9f0e7ad11ab812dd44d9df1
Adedamola Egbebi <egbebitimi121@gmail.com>
2025-07-20 00:29:07 +0100

    Update url names to better reflect the resource they access

--- bizease/orders/urls.py
+++ bizease/orders/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
 	path('', views.OrdersView.as_view(), name="orders"),
 	path('stats', views.OrderStatsView.as_view(), name="orders-stats"),
 	path('<int:order_id>', views.SingleOrderView.as_view(), name="order"),
-	path('<int:order_id>/ordered-products/<int:product_id>', views.OrderedProductView.as_view(), name="ordered-products"),
-	path('<int:order_id>/ordered-products', views.OrderedProducts.as_view(), name="ordered-product"),
+	path('<int:order_id>/ordered-products/<int:product_id>', views.SingleOrderedProductView.as_view(), name="ordered-product"),
+	path('<int:order_id>/ordered-products', views.OrderedProductsView.as_view(), name="ordered-products"),
 ]
